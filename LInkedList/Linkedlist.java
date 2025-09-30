f982ed0c057f57d19b458cd0d7849c0f477537d1
vikascselab <cse9565@gmail.com>
2025-09-30 13:36:20 +0530

    Link list queston

---
+++ LInkedList/Linkedlist.java
@@ -0,0 +1,19 @@
+
+public class Linkedlist {
+
+  public static class node {
+
+    int data;
+    node next;
+
+    public node(int data) {
+      this.data = data;
+      this.next = null;
+    }
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
