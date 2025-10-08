6e21081f284dd39a254bbaed467e6f8f45ce1ed7
vikascselab <cse9565@gmail.com>
2025-10-09 01:07:46 +0530

    Queue

---
+++ Queue/Implementation.java
@@ -0,0 +1,83 @@
+class Implementation {
+
+  static class Queue {
+
+    static int arr[];
+    static int size;
+    static int rear;
+
+    Queue(int n) {
+      arr = new int[n];
+
+      size = n;
+      rear = -1;
+    }
+
+    public static boolean isEmptyt() {
+      return rear == -1;
+    }
+
+    // add function
+
+    public static void add(int data) {
+      if (rear == size - 1) {
+        System.out.println("Queue is full");
+        return;
+
+      }
+
+      rear = rear + 1;
+      arr[rear] = data;
+    }
+
+    // remove function
+
+    public static int remove() {
+
+      if (isEmptyt()) {
+        System.out.println("empty queue ");
+        return -1;
+
+      }
+
+      int front = arr[0];
+      for (int i = 0; i < rear; i++) {
+
+        arr[i] = arr[i + 1];
+
+      }
+      rear = rear - 1;
+      return front;
+
+    }
+
+    // peek
+
+    public static int peek() {
+      if (isEmptyt()) {
+        System.out.println("empty queue");
+
+      }
+      return arr[0];
+    }
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println("welcome to queue");
+
+    Queue q = new Queue(5);
+
+    Queue.add(1);
+    q.add(2);
+    q.add(3);
+
+    while (!q.isEmptyt()) {
+      System.out.println(q.peek());
+      q.remove();
+
+    }
+
+  }
+
+}
