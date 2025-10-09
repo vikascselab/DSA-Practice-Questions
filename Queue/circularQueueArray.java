05c23b30e02db6d8e99508f8bc6f0bf698b83147
vikascselab <cse9565@gmail.com>
2025-10-09 16:46:30 +0530

    Queue

---
+++ Queue/circularQueueArray.java
@@ -0,0 +1,105 @@
+class circularQueueArray {
+
+  static class Queue {
+
+    static int arr[];
+    static int size;
+    static int rear;
+    static int front;
+
+    Queue(int n) {
+      arr = new int[n];
+
+      size = n;
+      rear = -1;
+      front = -1;
+    }
+
+    public static boolean isEmptyt() {
+      return rear == -1 && front == -1;
+    }
+
+    // full
+
+    public static boolean isFull() {
+      return (rear + 1) % size == front;
+    }
+
+    // add function
+
+    public static void add(int data) {
+      if (isFull()) {
+        System.out.println("Queue is full");
+        return;
+
+      }
+
+      // add first element
+      if (front == -1) {
+        front = 0;
+
+      }
+
+      rear = (rear + 1) % size;
+      arr[rear] = data;
+    }
+
+    // remove function
+
+    public static int remove() {
+
+      if (isEmptyt()) {
+        System.out.println("empty queue ");
+        return -1;
+
+      }
+
+      int result = arr[front];
+
+      if (rear == front) {
+        rear = front = -1;
+
+      } else {
+        front = (front + 1) % size;
+
+      }
+
+      return result;
+
+    }
+
+    // peek
+
+    public static int peek() {
+      if (isEmptyt()) {
+        System.out.println("empty queue");
+
+      }
+      return arr[front];
+    }
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println("welcome to queue");
+
+    Queue q = new Queue(5);
+
+    Queue.add(1);
+    q.add(2);
+    q.add(3);
+
+    System.out.println(q.remove());
+    q.add(4);
+    System.out.println(q.remove());
+    q.add(5);
+
+    while (!q.isEmptyt()) {
+      System.out.println(q.peek());
+      q.remove();
+
+    }
+
+  }
+
+}
