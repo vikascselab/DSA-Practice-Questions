bb4f5f587e11ba20ef5b749c0a5250f4dfaccf85
vikascselab <cse9565@gmail.com>
2025-10-23 12:14:16 +0530

    PQ

---
+++ PriorityQueue/slidedingWidows.java
@@ -0,0 +1,59 @@
+import java.util.PriorityQueue;
+
+class slidedingWidows {
+
+  static class Pair implements Comparable<Pair> {
+    int val;
+    int idx;
+
+    public Pair(int val, int idx) {
+      this.val = val;
+      this.idx = idx;
+    }
+
+    @Override
+    public int compareTo(Pair p2) {
+      return p2.val - this.val;
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println("welcomr to slidering windows");
+
+    int arr[] = { 1, 3, -1, -3, 5, 3, 6, 7 };
+
+    int k = 3; // this is windows size
+
+    int res[] = new int[arr.length - k + 1];
+
+    PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+    // 1st wwindows
+    for (int i = 0; i < k; i++) {
+
+      pq.add(new Pair(arr[i], i));
+
+    }
+
+    res[0] = pq.peek().val;
+
+    // rest
+    for (int i = k; i < arr.length; i++) {
+      while (pq.size() > 0 && pq.peek().idx <= (i - k)) {
+        pq.remove();
+
+      }
+
+      pq.add(new Pair(arr[i], i));
+      res[i - k + 1] = pq.peek().val;
+
+    }
+
+    // print of result
+    for (int i = 0; i < res.length; i++) {
+      System.out.println("result is " + res[i]);
+
+    }
+  }
+
+}
