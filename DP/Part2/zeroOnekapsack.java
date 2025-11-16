10dccf0379397528fcd7e1bfda416575560efb1c
vikascselab <cse9565@gmail.com>
2025-11-16 21:14:30 +0530

    Dp

---
+++ DP/Part2/zeroOnekapsack.java
@@ -0,0 +1,112 @@
+public class zeroOnekapsack {
+  // this is for recuesion
+  // public static int kanpsackR(int val[], int wt[], int W, int n) {
+  // if (W == 0 || n == 0) {
+  // return 0;
+
+  // }
+
+  // if (wt[n - 1] <= W) {
+  // // include
+  // int ans1 = val[n - 1] + kanpsackR(val, wt, W - wt[n - 1], n - 1);
+
+  // // exclude
+  // int ans2 = kanpsackR(val, wt, W, n - 1);
+
+  // return Math.max(ans1, ans2);
+  // } else {
+  // return kanpsackR(val, wt, W, n - 1);
+  // }
+  // }
+
+  // this is fo memoization
+
+  public static int kanpsackR(int val[], int wt[], int W, int n, int dp[][]) {
+    if (W == 0 || n == 0) {
+      return 0;
+
+    }
+
+    if (dp[n][W] != -1) {
+      return dp[n][W];
+
+    }
+
+    if (wt[n - 1] <= W) {
+      // include
+      int ans1 = val[n - 1] + kanpsackR(val, wt, W - wt[n - 1], n - 1, dp);
+
+      // exclude
+      int ans2 = kanpsackR(val, wt, W, n - 1, dp);
+
+      dp[n][W] = Math.max(ans1, ans2);
+      return dp[n][W];
+    } else {
+      dp[n][W] = kanpsackR(val, wt, W, n - 1, dp);
+
+      return dp[n][W];
+
+    }
+  }
+
+  // tabulation
+
+  public static int kanpsackT(int val[], int wt[], int W) {
+    int n = val.length;
+    int dp[][] = new int[n + 1][W + 1];
+    for (int i = 0; i < dp.length; i++) {
+      dp[i][0] = 0;
+
+    }
+
+    for (int j = 0; j < dp[0].length; j++) {
+      dp[0][j] = 0;
+
+    }
+
+    for (int i = 1; i < n + 1; i++) {
+      for (int j = 1; j < W + 1; j++) {
+        int v = val[i - 1];
+        int w = wt[i - 1];
+
+        if (w <= j) {
+          int iPro = v + dp[i - 1][j - w];
+          int exPro = dp[i - 1][j];
+          dp[i][j] = Math.max(iPro, exPro);
+        } else {
+          int exPro = dp[i - 1][j];
+          dp[i][j] = exPro;
+
+        }
+
+      }
+
+    }
+    return dp[n][W];
+  }
+
+  public static void main(String[] args) {
+    System.out.println("welcome to 0/1 kanpsack problem");
+
+    int val[] = { 15, 14, 10, 45, 30 };
+    int wt[] = { 2, 5, 1, 3, 4 };
+    int W = 7;
+
+    // int dp[][] = new int[val.length + 1][W + 1];
+
+    // for (int i = 0; i < dp.length; i++) {
+    // for (int j = 0; j < dp[0].length; j++) {
+
+    // dp[i][j] = -1;
+
+    // }
+    // }
+    // System.out.println(kanpsackR(val, wt, W, val.length, dp));
+
+    // System.out.println(kanpsackR(val, wt, W, val.length));
+
+    System.out.println(kanpsackT(val, wt, W));
+
+  }
+
+}
