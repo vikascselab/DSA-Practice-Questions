433047d6b2dfcf67081e5b8868afa259bb39c702
vikascselab <cse9565@gmail.com>
2025-11-25 12:29:53 +0530

    dp

--- DP/Part6/minimumpartitionzerokanp.java
+++ DP/Part6/minimumpartitionzerokanp.java
@@ -1,7 +1,41 @@
 public class minimumpartitionzerokanp {
 
+  public static int minPartition(int arr[]) {
+
+    int n = arr.length;
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+
+    }
+    int W = sum / 2;
+
+    int dp[][] = new int[n + 1][W + 1];
+
+    // bottom up
+    for (int i = 1; i < n + 1; i++) {
+      for (int j = 1; j < W + 1; j++) {
+        if (arr[i - 1] <= j) {
+          dp[i][j] = Math.max(arr[i - 1] + dp[i - 1][j - arr[i - 1]], dp[i - 1][j]);
+
+        } else {
+          dp[i][j] = dp[i - 1][j];
+        }
+      }
+    }
+    int sum1 = dp[n][W];
+    int sum2 = sum - sum1;
+
+    return Math.abs(sum1 - sum2);
+
+  }
+
   public static void main(String[] args) {
     System.out.println("welcome to minimum partition in (zero kapsack probleam based)");
+
+    int num[] = { 1, 6, 11, 5 };
+
+    System.out.println(minPartition(num));
   }
 
 }
