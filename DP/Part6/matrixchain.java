698f8161bc1c20f5654af6f52bd9f6700a9e9bb0
vikascselab <cse9565@gmail.com>
2025-11-25 11:52:31 +0530

    dp

--- DP/Part6/matrixchain.java
+++ DP/Part6/matrixchain.java
@@ -52,21 +52,53 @@ public class matrixchain {
     return dp[i][j] = ans;
   }
 
+  // tabulation
+
+  public static int mcbTab(int arr[]) {
+    int n = arr.length;
+
+    int dp[][] = new int[n][n];
+
+    for (int i = 0; i < n; i++) {
+      dp[i][i] = 0;
+    }
+
+    // bottom up
+
+    for (int len = 2; len <= n - 1; len++) {
+      for (int i = 1; i <= n - len; i++) {
+        int j = i + len - 1;
+        dp[i][j] = Integer.MAX_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+          int cost1 = dp[i][k];
+          int cost2 = dp[k + 1][j];
+          int cost3 = arr[i - 1] * arr[k] * arr[j];
+          dp[i][j] = Math.min(dp[i][j], cost1 + cost2 + cost3);
+        }
+      }
+
+    }
+
+    return dp[1][n - 1];
+  }
+
   public static void main(String[] args) {
     System.out.println("welcome to mcm");
 
     int arr[] = { 1, 2, 3, 4, 3 };
-    int n = arr.length;
+    // int n = arr.length;
 
     // System.out.println(mcm(arr, 1, n - 1));
 
-    int dp[][] = new int[n][n];
-    for (int i = 0; i < n; i++) {
-      Arrays.fill(dp[i], -1);
+    // int dp[][] = new int[n][n];
+    // for (int i = 0; i < n; i++) {
+    // Arrays.fill(dp[i], -1);
 
-    }
+    // }
 
-    System.out.println(mcmM(arr, 1, n - 1, dp));
+    // System.out.println(mcmM(arr, 1, n - 1, dp));
+
+    System.out.println(mcbTab(arr));
 
   }
 
---
+++ DP/Part6/minimumpartitionzerokanp.java
@@ -0,0 +1,7 @@
+public class minimumpartitionzerokanp {
+
+  public static void main(String[] args) {
+    System.out.println("welcome to minimum partition in (zero kapsack probleam based)");
+  }
+
+}
